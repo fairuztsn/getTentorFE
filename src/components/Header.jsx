937bc4843f39976f0d8313a367d27ac7a05fb0cb
fae <101633871+fairuztsn@users.noreply.github.com>
2025-05-29 07:33:26 +0700

    feat: header set clickable image with navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 // src/components/Header.jsx
 import { useUser } from "@/contexts/UserContextProvider";
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const { user } = useUser();
+  const navigate = useNavigate();
+
   return (
     <header className="bg-light-blue py-4 px-6 flex justify-between items-center shadow">
       <div className="flex items-center space-x-2">
-        <img src={`/public/images/gettentor.png`} alt="logo" className="w-64 h-12" />
+        <img src={`/public/images/gettentor.png`} alt="logo" className="w-64 h-12" onClick={() => navigate('/')} style={{cursor: 'pointer'}}/>
       </div>
       <div className="flex items-center space-x-4">
         <button className="bg-blue-dark hover:bg-blue-600 text-white px-4 py-1 rounded">Tentor Favorit</button>
